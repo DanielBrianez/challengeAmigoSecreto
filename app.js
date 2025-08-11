02c8af806cbc6105d40186b36ac41cf823deb9da
Daniel Nascimento <you@example.comgit config --global user.email you@example.com>
2025-08-11 00:46:23 -0300

    Check-up, refatoração de possiveis partes repetitivas, correção da função de verificarAmigo para adicionarAmigo e testes. Aprovado pelo QA - Ready for Deploy

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
 
-function verificarAmigos(){
+function adicionarAmigo(){
     let input = document.getElementById('amigo');
     let nome = input.value.trim();
 
