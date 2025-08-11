2b7adbf48f69b31c38d64bd7faf4817c78528f48
Daniel Nascimento <you@example.comgit config --global user.email you@example.com>
2025-08-11 00:10:16 -0300

    Criada a função de verificar o nome adicionado, verificar se o campo está vazio, adicionar o nome a uma lista e limpar o campo depois de adicionado o nome

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function verificarAmigos(){
+    let input = document.getElementById('amigo');
+    let nome = input.value.trim();
+
+    if (nome == ''){
+        alert('Insira um nome válido!');
+        return;
+    }
+
+    amigos.push(nome)
+    input.value = '';
+    }
